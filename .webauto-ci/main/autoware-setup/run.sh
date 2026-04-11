ec36a337dbbe3345ae927cd70013bcdbbee19d25
Shigekazu Fukuta <107168699+sfukuta@users.noreply.github.com>
2026-04-11 09:41:14 +0900

    update autoware-setup/run.sh

--- .webauto-ci/main/autoware-setup/run.sh
+++ .webauto-ci/main/autoware-setup/run.sh
@@ -5,7 +5,7 @@ ansible_args+=("--extra-vars" "prompt_install_nvidia=y")
 ansible_args+=("--extra-vars" "prompt_download_artifacts=y")
 ansible_args+=("--extra-vars" "data_dir=$HOME/autoware_data")
 ansible_args+=("--extra-vars" "ros2_installation_type=ros-base")
-ansible_args+=("--extra-vars" "install_devel=false")
+ansible_args+=("--extra-vars" "install_devel=y")
 
 # read amd64 env file and expand ansible arguments
 source 'amd64.env'
